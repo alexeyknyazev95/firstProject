6530aa559cb4ec2691eba96744633c47d6a0e4d2
aknyazev <knyazevalexey1995@gmail.com>
2020-01-05 15:09:45 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+console.log(leftBorderWidth);  
+var leftBorderWidth = 1;
+
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);  
+console.log(second);
+console.log(pi);
+
+console.log('str'*9);
+
+let person = {
+    name: "John",
+    age: 35,
+    isMarried: false
+}
+
+console.log(person.age);
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bnp'];
+console.log(arr[2]);
+
+//alert("Hello, world!");
+//let answer = confirm("Хочешь?");
+//console.log(answer);*/
+
+// let answer = +prompt("Возраст?", 18);
+// console.log(typeof(answer));
+
+
+// console.log('add', 'ass');
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr);
+console.log(decr);
+
+console.log(5/2);
+console.log(true && false);
